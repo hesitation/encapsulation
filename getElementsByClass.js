0106796df772d54ed6401ac8dc8674c5e3251197
Zhang Wen <kyxylmd@icloud.com>
2017-01-08 16:14:08 +0800

    封装getElementsByClassName()
    由于IE6、7、8没有getElementsByClassName()方法，通过自己封装实现，可以兼容多个属性值得情况

---
+++ getElementsByClass.js
@@ -0,0 +1,34 @@
+/**
+ * 检查节点是否包含cls属性（应为一个节点可能会有多个class属性值，以空格分隔，其className值是一个长字符串）
+ * @param  {[节点]}  ele [description]
+ * @param  {[属性值]}  cls [description]
+ * @return {Boolean}     [description]
+ */
+function hasClass(ele, cls) {
+  var clsArray = ele.className.split(/\s+/);     //将属性值的字符串以空格分隔为字符串数组，\s+匹配一个或多个空格
+  for(var j=0; j<clsArray.length; j++) {
+    if(clsArray[j] === cls) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+/**
+ * 为IE6、7、8封装getElementsByClassName()函数
+ * @param  {[节点]} obj [需要查找的节点]
+ * @param  {[属性值]} cls [class属性值]
+ * @return {[type]} array [返回匹配的DOM元素数组]
+ */
+function getByClassName(ele, cls) {
+  var elements = obj.getElementsByTagName("*"); //获取obj节点内所有后代节点
+  var resluts = [];                             //保存获取到的节点数组
+
+  for(var i=0; i<elements.length; i++) {
+    if(hasClass(elements[i], cls)) {
+      resluts.push(elements[i]);
+    }
+  }
+  return resluts;
+}
