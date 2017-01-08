c8f0c131887b92f919c64825c0430709cd2c6f56
Zhang Wen <kyxylmd@icloud.com>
2017-01-08 17:01:47 +0800

    Update getElementsByClass.js

--- getElementsByClass.js
+++ getElementsByClass.js
@@ -1,8 +1,8 @@
 /**
  * 检查节点是否包含cls属性（应为一个节点可能会有多个class属性值，以空格分隔，其className值是一个长字符串）
- * @param  {[节点]}  ele [description]
- * @param  {[属性值]}  cls [description]
- * @return {Boolean}     [description]
+ * @param  {[节点]}  ele [检查的节点]
+ * @param  {[属性值]}  cls [要查找的class属性值]
+ * @return {Boolean}     [找到返回true;否则false]
  */
 function hasClass(ele, cls) {
   var clsArray = ele.className.split(/\s+/);     //将属性值的字符串以空格分隔为字符串数组，\s+匹配一个或多个空格
